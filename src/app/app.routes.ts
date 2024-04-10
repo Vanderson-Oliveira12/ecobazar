3489877804624d7a6c8c5e9e29d7c2db4d6ec553
Vanderson-Oliveira12 <vanderson.doliveira14@gmail.com>
2024-04-10 14:41:37 -0300

    adjusting header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: "",
+    pathMatch: 'full',
+    loadComponent: () => import("./pages/home/home.component").then(x => x.HomeComponent)
+  },
+  {
     path: "auth",
     loadChildren: () => import("./pages/auth/routes").then(x => x.AUTH_ROUTES)
   },
